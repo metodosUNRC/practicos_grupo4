5824c6e72cc1e799d9661d2eb5ef671ee17cc9a3
Agustin <31513268+agugonzalez@users.noreply.github.com>
2017-10-02 11:35:38 -0300

    Modificacion 2
    cambie la parte de las iteraciones por el error

--- Practico 3/jacobi.m
+++ Practico 3/jacobi.m
@@ -1,10 +1,14 @@
 clear all
 
+format long
+
 A=input('ingrese una matriz deseada: ');
 b=input('ingrese el lado derecho b: ');
-e=input('ingrese el numero de interaciones: ');
+e=input('ingrese el error deseado: ');
 
 [m,n]=size(A);%esta funcion me da la cantidad de filas y columnas que tengo
+
+g=inv(A)*b;
 
 if m==n%condicion para saber si tenemos el mismo numero de columnas y filas
     D=eye(m,n);
@@ -18,14 +22,17 @@ U=((-1)*triu(A))+D;
 z(m,1)=0;
 P=(O*b);
 W=(O*(L+U));
-k=1;
-while e>=k
+x(1,1)=100;
+k=0;
+while abs(g(1,1)-x(1,1))>e
     x=P+W*z;
     z=x;
     k=k+1;
 end
 disp('la aproximación encontrada es');
 disp(x);
+disp('el numero de iteraciones es: ');
+disp(k);
 else
     disp('error la matriz no es cuadrada');
 end
